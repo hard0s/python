53ca1a833a0996b0495a364eea5e1547842fb34d
EGAProg <gameswithiy@gmail.com>
2023-01-25 10:06:57 +0300

    The most hatred living - BUG

--- findMaxFunc.py
+++ findMaxFunc.py
@@ -1,14 +1,9 @@
-def biggest(* args):
-    max = args[0]
-    for item in args[1:]:
+def biggest(a):
+    max = a[0]
+    for item in a[1:]:
         if item > max:
             max = item
-    return max
+    print("Наибольшее число: ", max)
 
 a = list(map(int,input("Введите числа в массив: ").split(' ')))
-
-b = biggest(a)
-
-str(b)
-
-print(b)
+biggest(a)

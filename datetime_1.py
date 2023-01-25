0ec6300316ec3372ee95963b7c5175b1a970e1b4
EGAProg <gameswithiy@gmail.com>
2023-01-25 11:29:35 +0300

    Date time funcs

---
+++ datetime_1.py
@@ -0,0 +1,8 @@
+import datetime
+
+date_time = datetime.datetime.now()
+current_time = date_time.time()
+print(current_time)
+print(datetime.datetime(2023, 3, 10))
+td_object = datetime.timedelta(days=5, seconds=5, milliseconds=15)
+print(td_object) 

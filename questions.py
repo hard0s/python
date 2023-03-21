3142f8935dfae3c905831fb5af72f2c5a634f7ad
EGAProg <gameswithiy@gmail.com>
2023-03-21 12:00:55 +0300

    WTF?

---
+++ questions.py
@@ -0,0 +1,16 @@
+import random
+
+with open('test.txt') as f:
+    lines = f.readlines()
+
+
+while lines:
+
+    qa_pairs = [line.strip().split('|') for line in lines]
+    question, answer = random.choice(qa_pairs)
+    user_answer = input(question + '\n')
+
+    if user_answer.strip().lower() == answer.lower():
+        print('Correct!')
+    else:
+        print(f'Sorry, the correct answer is "{answer}".')

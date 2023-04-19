8ded28a35f9212015bc15afb4f8d9230b46d4a41
EGAProg <gameswithiy@gmail.com>
2023-04-19 10:46:23 +0300

    Modules and packages

--- modules/modul.py
+++ modules/modul.py
@@ -1,1 +1,11 @@
-import findMaxFunc
+from packages import filesFunc 
+from packages import datetime_1 
+from packages import celend
+# file functions
+filename = str(input("Enter filename: "))
+f = filesFunc.fileFunc(filename)
+print(f)
+# datetime functions
+datetime_1.currTime()
+# celendar function
+celend.cel()

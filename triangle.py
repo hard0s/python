09587ede2c23d7d5f9c0542e518d7433979d4cc7
EGAProg <gameswithiy@gmail.com>
2023-04-25 09:25:02 +0300

    Unit Test fixed

--- triangle.py
+++ triangle.py
@@ -1,14 +1,16 @@
 class Triangle:
     def Logic(a, b, c):
         if (a > 0 and b > 0 and c > 0):
-            if (a + b > c or b + c > a or c + a > b):
+            if (a + b > c and b + c > a and c + a > b):
                 if (a == b and a == c and c == b):
-                    return ("Equilateral tiangle") # Side A = Side B ond Side A = Side C -- equilateral tiangle
-                if (a == b) or (a == c) or (c == b):
-                    return("Isosceles triange") # side a = side b or side b = side c or side a = side c -- isosceles triange
+                    print("Equilateral tiangle") # Side A = Side B ond Side A = Side C -- equilateral tiangle
+                elif (a == b) or (a == c) or (c == b):
+                    print("Isosceles triange") # side a = side b or side b = side c or side a = side c -- isosceles triange
                 else: 
-                    return("Versatile triangle") # Versatile Triangle
-            
+                    print("Versatile triangle") # Versatile Triangle  
+            else:
+                print("Theres no triangle like this")
         else:
-            return("Theres no triangle like this")
+            print("Theres no triangle like this")
 
+Triangle.Logic(33, 0, 0)

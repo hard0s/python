e68cef96f5ebb88e58b5d6729e0c80373134d5f2
OPPYM <gameswithiy@gmail.com>
2023-06-02 08:50:41 +0300

    2048 GAME

--- 2048/main.py
+++ 2048/main.py
@@ -1,3 +1,4 @@
+# GPL 3.2 License author Egor Gysev. Contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
 from tkinter import *
 from creational.Board import Board
 from creational.Game import Game

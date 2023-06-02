f33efc1e7c89c2bed7b2455756f1c2dad502d2cb
OPPYM <gameswithiy@gmail.com>
2023-06-02 11:07:43 +0300

    main Game

--- 2048/main.py
+++ 2048/main.py
@@ -1,7 +1,6 @@
-# GPL 3.2 License author Egor Gysev. Contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
 from tkinter import *
-from creational.Board import Board
 from creational.Game import Game
+from creational.Board import Board
 
 gamepanel =Board()
 game2048 = Game( gamepanel)

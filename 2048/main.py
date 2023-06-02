71a3f354de0557f7195e6e7569173fa316d529d2
OPPYM <gameswithiy@gmail.com>
2023-06-02 08:53:21 +0300

    Git merge

---
+++ 2048/main.py
@@ -0,0 +1,8 @@
+# GPL 3.2 License author Egor Gysev. Contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+from tkinter import *
+from creational.Board import Board
+from creational.Game import Game
+
+gamepanel =Board()
+game2048 = Game( gamepanel)
+game2048.start()

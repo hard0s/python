2f9cfa84b384c442d93d897aa09c493a76d5899d
EGAProg <gameswithiy@gmail.com>
2023-06-06 15:00:57 +0300

    --no-responce

--- sql/main.py
+++ sql/main.py
@@ -1,14 +1,34 @@
 import mysql.connector
 from mysql.connector import Error
 
-def create_connection(host_name, user_name, user_password):
+def create_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
-        connection = mysql.connector.connect(
-            host="127.0.0.1",
-            user="root",
-            passwd="ytevt.ghblevsdfnmgfhjkb"
+        return mysql.connector.connect(
+            host=host_name, 
+            user=user_name, 
+            password=user_password,
+            database=db_name
         )
-        print("Connection to MySQL DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        result = str(cursor.fetchall())
+        fb = open("database.json", "w")
+        fb.write(result)
+        fb.close()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def main():
+    connect = create_connection("127.0.0.1", "root", "ytevt.ghblevsdfnmgfhjkb", "zavod")
+    query = str(input("Enter query: "))
+    print(create_database(connect, query))
+
+if __name__ == "__main__":
+    main()
